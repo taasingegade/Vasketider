6649a0eb2123a06f4d4cfe4fc74d86b12b66b2bd
taasingegade <hornsbergmorten@gmail.com>
2025-07-09 23:09:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -261,14 +261,12 @@ def index():
     alle_14 = cur.fetchall()
     conn.close()
 
-  bookinger = {}
+    bookinger = {}
     for b in alle_14:
-        dato_obj = b[2]
-        if isinstance(dato_obj, str):
-            try:
-                dato_obj = datetime.strptime(dato_obj, "%Y-%m-%d")
-            except ValueError:
-                dato_obj = datetime.strptime(dato_obj, "%d-%m-%Y")
+        try:
+            dato_obj = datetime.strptime(str(b[2]), '%d-%m-%Y')
+        except ValueError:
+            dato_obj = datetime.strptime(str(b[2]), '%Y-%m-%d')
         dato_str = dato_obj.strftime('%d-%m-%Y')
         bookinger[(dato_str, b[3])] = b[1]
 
